2468659183ca7a7f7be72cbf6799833bec57984f
Sunmyeong Jin <tjsaud3250@naver.com>
2024-02-25 21:09:16 +0900

    controller jwt 토큰 수정

--- src/main/java/com/cos/blog/controller/api/BoardApiController.java
+++ src/main/java/com/cos/blog/controller/api/BoardApiController.java
@@ -3,12 +3,7 @@ package com.cos.blog.controller.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.cos.blog.config.auth.PrincipalDetail;
 import com.cos.blog.dto.ResponseDto;
@@ -22,23 +17,26 @@ public class BoardApiController {
 	private BoardService boardService;
 	
 	@PostMapping("/api/board")
-	public ResponseDto<Integer> save(@RequestBody Board board, @AuthenticationPrincipal PrincipalDetail principal) {
-		boardService.글쓰기(board, principal.getUser());
+	public ResponseDto<Integer> save(@RequestBody Board board, @RequestHeader("Authorization") String accessToken) {
+		accessToken=accessToken.substring(7);
+		boardService.글쓰기(board, accessToken);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); 
 	}
 	
 	@DeleteMapping("/api/board/{id}")
-	public ResponseDto<Integer> deleteById(@PathVariable int id){
-		boardService.글삭제하기(id);
+	public ResponseDto<Integer> deleteById(@RequestHeader("Authorization") String accessToken, @PathVariable int id){
+		accessToken=accessToken.substring(7);
+		boardService.글삭제하기(id, accessToken);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); 
 	}
 	
 	@PutMapping("/api/board/{id}")
-	public ResponseDto<Integer> update(@PathVariable int id, @RequestBody Board board){
+	public ResponseDto<Integer> update(@PathVariable int id,@RequestHeader("Authorization") String accessToken, @RequestBody Board board){
+		accessToken=accessToken.substring(7);
 		System.out.println("BoardApiController : update : id : "+id);
 		System.out.println("BoardApiController : update : board : "+board.getTitle());
 		System.out.println("BoardApiController : update : board : "+board.getContent());
-		boardService.글수정하기(id, board);
+		boardService.글수정하기(id, board, accessToken);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 	
